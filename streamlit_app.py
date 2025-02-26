6ddcf5a496990c6a798ca223ac89c0521c4f108e
vanducngo <vanducngo@users.noreply.github.com>
2025-02-26 08:07:47 +0000

    F

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
+from langchain_core.embeddings import Embeddings
 import streamlit as st
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai.chat_models import ChatOpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from langchain_community.vectorstores import Chroma
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.prompts import ChatPromptTemplate
 
 PROMPT_TEMPLATE = """
@@ -18,51 +19,53 @@ Answer the question based on the above context: {question}
 """
 
 def generate_response(input_text):
-    # Select embedding
-    embeddings = GoogleGenerativeAIEmbeddings(google_api_key=google_api_key)
-    # Vector store
-    db = Chroma(embedding_function=embeddings)  
-    # Search the DB
-    results = db.similarity_search_with_relevance_scores(input_text, k=3)
-    if len(results) == 0 or results[0][1] < 0.7:
-        st.info("Không tìm thấy kết quả nào ")      
-    else:
-        context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-        prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-        prompt = prompt_template.format(context=context_text, question=input_text)
-        #st.info(prompt)
-        model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.7, google_api_key=google_api_key)
-        st.info(model.invoke(prompt).content)
+	# Select embedding
+	embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key)
+	# Vector store
+	db = Chroma(embedding_function=embeddings)  
+	# Search the DB
+	results = db.similarity_search_with_relevance_scores(input_text, k=3)
+	if len(results) == 0 or results[0][1] < 0.7:
+		st.info("Không tìm thấy kết quả nào ")      
+	else:
+		context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+		prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+		prompt = prompt_template.format(context=context_text, question=input_text)
+		#st.info(prompt)
+		model=ChatOpenAI(temperature=0.7, api_key = openai_api_key)
+		st.info(model.invoke(prompt).content)
+	
+	
 
 # Page title
 st.title = "FINAL PROJECT"
-google_api_key = st.sidebar.text_input("Google Gemini API Key", type="password")
+openai_api_key = st.sidebar.text_input("OpenAI API Key","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", type="password")
 
 # File upload
-uploaded_file = st.file_uploader('Upload your file:', type='txt')
+uploaded_file=st.file_uploader('Upload your file:',type='txt')
 if st.button("Load Data"):
-    # Load document if file is uploaded
-    if uploaded_file is not None:
-        documents = [uploaded_file.read().decode()]
-        # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=100)
-        chunks = text_splitter.create_documents(documents)
-        
-        # Select embedding
-        embeddings = GoogleGenerativeAIEmbeddings(google_api_key=google_api_key)
-        # Create vector store from document
-        db = Chroma.from_documents(chunks, embeddings)    
-        'Data Load OK'
+	#Load document if file is uploaded
+	if uploaded_file is not None:
+		documents=[uploaded_file.read().decode()]
+		# Split documents into chunks
+		#text_splitter = RecursiveCharacterTextSplitter(chunk_size=300,chunk_overlap=100,length_function=len,add_start_index=True)
+		text_splitter = CharacterTextSplitter(chunk_size=300,chunk_overlap=100)
+		chunks = text_splitter.create_documents(documents)
+		#print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+		# Select embedding
+		embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key)
+		# Create vector store from document
+		db = Chroma.from_documents(chunks,embeddings)    
+		'Data Load OK'
 
 with st.form("my_form"):
-    text = st.text_area(
-        "Enter text:",
-        "Bạn muốn hỏi gì?",
-    )
-    submitted = st.form_submit_button("Submit", disabled=not uploaded_file)
-    if not google_api_key:
-        st.warning("Please enter your Google Gemini API Key", icon=None)
-    if submitted and google_api_key:
-        generate_response(text)
-
-
+	text=st.text_area(
+		"Enter text:",
+		"Bạn muốn hỏi gì?",
+		)
+	submitted= st.form_submit_button("Submit",disabled= not(uploaded_file))
+	if not openai_api_key.startswith("sk"):
+		st.warning("Please enter your OpenAI API Key", icon=None)
+	if submitted and openai_api_key.startswith("sk-"):
+		generate_response(text)
+	
